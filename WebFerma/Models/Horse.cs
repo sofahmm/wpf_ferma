eb0b26aa3849d5b283e38c8d65112fce641f2b88
sofahmm <74815523+sofahmm@users.noreply.github.com>
2021-12-12 12:59:34 +0300

    commit

---
+++ WebFerma/Models/Horse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFerma.Models
+{
+    public class Horse
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Age_of_the_foal { get; set; }
+    }
+}
---
+++ WebFerma/Models/Horse_health.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFerma.Models
+{
+    public class Horse_health
+    {
+        public int ID { get; set; }
+        public int ID_Horse { get; set; }
+        public int Weight { get; set; }
+        public int Body_temperature{ get; set; }
+        public string Dental_condition { get; set; }
+        public DateTime DataReport { get; set; }
+    }
+}
